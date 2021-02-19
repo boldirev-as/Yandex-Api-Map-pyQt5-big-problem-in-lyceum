e95f6e8965388feac8a34a4612ee87ff6fdf2d95
boldirev-as <60057518+boldirev-as@users.noreply.github.com>
2021-02-19 16:17:04 +0500

    Часть №2

--- map_geo_coords_api.py
+++ map_geo_coords_api.py
@@ -3,10 +3,10 @@ import requests
 STATIC_API_SERVER = "https://static-maps.yandex.ru/1.x/"
 
 
-def get_map_from_coords(coords, type_in=str):
+def get_map_from_coords(coords, type_in=str, spn=(0.025, 0.025)):
     params = {
         "ll": coords if type_in is str else ",".join(coords),
-        "spn": "0.025,0.025",
+        "spn": ",".join(map(str, spn)),
         "l": "map"
     }
 
@@ -14,4 +14,5 @@ def get_map_from_coords(coords, type_in=str):
                                 params=params)
     if response.status_code == 200:
         return response.content
+    print(response.status_code)
     return None
--- Большая задача по Maps API.py
+++ Большая задача по Maps API.py
@@ -1,6 +1,7 @@
 import sys
 
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 
 from map_geo_coords_api import get_map_from_coords
 
@@ -11,14 +12,33 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.spn = [0.0025, 0.0025]
+        self.min_spn = [0.0005, 0.0005]
+
         uic.loadUi('graphic.ui', self)
         self.search_btn.clicked.connect(self.search)
 
     def search(self):
-        image = get_map_from_coords(reversed(self.line_enter_coords.text().replace(" ", "").split(",")), type_in=list)
+        image = get_map_from_coords(
+            reversed(self.line_enter_coords.text().replace(" ", "").split(",")),
+            type_in=list,
+            spn=self.spn
+        )
         pixmap = QPixmap()
         pixmap.loadFromData(image)
         self.map.setPixmap(pixmap)
+
+    def keyPressEvent(self, event):
+
+        if event.key() in [Qt.Key_PageUp, Qt.Key_Up]:
+            self.spn[0] += 0.001
+            self.spn[1] += 0.001
+            self.search()
+        elif event.key() in [Qt.Key_PageDown, Qt.Key_Down]:
+            self.spn[0] = max(self.spn[0] - 0.001, self.min_spn[0])
+            self.spn[1] = max(self.spn[1] - 0.001, self.min_spn[1])
+            self.search()
 
 
 def except_hook(cls, exception, traceback):
