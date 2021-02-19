6cbba6936f1d5db9b64d77d0dca80431a50656de
boldirev-as <60057518+boldirev-as@users.noreply.github.com>
2021-02-19 19:18:47 +0500

    Часть №4

--- map_geo_coords_api.py
+++ map_geo_coords_api.py
@@ -3,11 +3,11 @@ import requests
 STATIC_API_SERVER = "https://static-maps.yandex.ru/1.x/"
 
 
-def get_map_from_coords(coords, type_in=str, spn=(0.025, 0.025)):
+def get_map_from_coords(coords, type_in=str, spn=(0.025, 0.025), l="map"):
     params = {
-        "ll": coords if type_in is str else ",".join(coords),
+        "ll": coords if type_in is str else ",".join(map(str, coords)),
         "spn": ",".join(map(str, spn)),
-        "l": "map"
+        "l": l
     }
 
     response = requests.request(method="GET", url=STATIC_API_SERVER,
--- Большая задача по Maps API.py
+++ Большая задача по Maps API.py
@@ -6,7 +6,7 @@ from PyQt5.QtCore import Qt
 from map_geo_coords_api import get_map_from_coords
 
 from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox
 
 
 class MyWidget(QMainWindow):
@@ -15,29 +15,55 @@ class MyWidget(QMainWindow):
 
         self.spn = [0.0025, 0.0025]
         self.min_spn = [0.0005, 0.0005]
+        self.place = [int, [0, 0]]
+        self.types_maps = {"спутник": "sat", "гибрид": "sat,skl", "схема": "map"}
 
         uic.loadUi('graphic.ui', self)
-        self.search_btn.clicked.connect(self.search)
+        self.search_btn.clicked.connect(self.clicked_btn_search)
+        self.type_map_box.currentTextChanged.connect(self.search)
+
+    def get_new_coords(self):
+        self.place[1] = \
+            list(reversed(list(map(float, self.line_enter_coords.text().replace(" ", "").split(",")))))
+
+    def clicked_btn_search(self):
+        try:
+            self.get_new_coords()
+        except Exception as e:
+            print(e)
+        self.search()
 
     def search(self):
         image = get_map_from_coords(
-            reversed(self.line_enter_coords.text().replace(" ", "").split(",")),
-            type_in=list,
-            spn=self.spn
+            self.place[1],
+            type_in=self.place[0],
+            spn=self.spn,
+            l=self.types_maps[self.type_map_box.currentText()]
         )
         pixmap = QPixmap()
         pixmap.loadFromData(image)
         self.map.setPixmap(pixmap)
 
     def keyPressEvent(self, event):
-
-        if event.key() in [Qt.Key_PageUp, Qt.Key_Up]:
+        flag = True
+        if event.key() == Qt.Key_PageUp:
             self.spn[0] += 0.001
             self.spn[1] += 0.001
-            self.search()
-        elif event.key() in [Qt.Key_PageDown, Qt.Key_Down]:
+        elif event.key() == Qt.Key_PageDown:
             self.spn[0] = max(self.spn[0] - 0.001, self.min_spn[0])
             self.spn[1] = max(self.spn[1] - 0.001, self.min_spn[1])
+        elif event.key() == Qt.Key_Down:
+            self.place[1][1] -= 0.001
+        elif event.key() == Qt.Key_Up:
+            self.place[1][1] += 0.001
+        elif event.key() == Qt.Key_Left:
+            self.place[1][0] -= 0.001
+        elif event.key() == Qt.Key_Right:
+            self.place[1][0] += 0.001
+        else:
+            flag = False
+
+        if flag:
             self.search()
 
 
